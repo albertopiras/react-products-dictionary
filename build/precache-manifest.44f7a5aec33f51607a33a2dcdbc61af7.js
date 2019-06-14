ec8009fb56bc8ce87a301e00a4657c6f9cfb94e4
Alberto Piras <apiras@google.com>
2019-06-14 15:02:08 +0200

    init gh-oages

---
+++ build/precache-manifest.44f7a5aec33f51607a33a2dcdbc61af7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c3eed1369f5d54cf3e7596ef46979f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "587cffac8e8e7c7a8f2d",
+    "url": "/static/css/main.468b60a5.chunk.css"
+  },
+  {
+    "revision": "e4a8b5b315be59302a77",
+    "url": "/static/js/2.98a30f42.chunk.js"
+  },
+  {
+    "revision": "587cffac8e8e7c7a8f2d",
+    "url": "/static/js/main.ac749401.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
